f6eca423627a2813f6e9199cfb5e5e07028ebdd0
Peter Halliday <peter.halliday@servicenow.com>
2023-11-07 13:50:24 -0600

    Add in tool versions and change permissions for Resource Group

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "azurerm_role_assignment" "rg_aad_group" {
   for_each = toset(var.terraform_state_aad_principals["groups"])
 
   scope                = data.azurerm_resource_group.state.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Reader"
   principal_id         = data.azuread_group.terraform_state_aad_group[each.value].object_id
 }
 
@@ -65,7 +65,7 @@ resource "azurerm_role_assignment" "rg_aad_users" {
   for_each = toset(var.terraform_state_aad_principals["users"])
 
   scope                = data.azurerm_resource_group.state.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Reader"
   principal_id         = data.azuread_user.terraform_state_aad_users[each.value].object_id
 }
 
@@ -73,7 +73,7 @@ resource "azurerm_role_assignment" "rg_aad_sps" {
   for_each = toset(var.terraform_state_aad_principals["service_principals"])
 
   scope                = data.azurerm_resource_group.state.id
-  role_definition_name = "Contributor"
+  role_definition_name = "Reader"
   principal_id         = data.azuread_service_principal.terraform_state_aad_sps[each.value].object_id
 }
 
